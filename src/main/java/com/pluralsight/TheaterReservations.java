607349d5b2de100bab4fb70f2368d915b15c34c9
Mario-Code7 <marioroa71@yahoo.com>
2025-10-01 12:32:00 -0500

    exercise 4 complete refractor

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -10,21 +10,23 @@ public class TheaterReservations {
 
         System.out.print("Please enter your name: ");
         String name = myScanner.nextLine();
+        String[] nameSplit = name.split(" ");
+        String firstName = nameSplit[0];
+        String lastName = nameSplit.length > 1 ? nameSplit[1] : "";
         System.out.println();
 
         System.out.print("What date will you be coming today(MM/dd/yyyy): ");
         String date = myScanner.nextLine();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/dd/yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         LocalDate dateComing = LocalDate.parse(date, formatter);
+        String formattedDate = dateComing.format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
         System.out.println();
 
         System.out.print("How many tickets would you like: ");
         int ticketAmount = myScanner.nextInt();
         System.out.println();
 
-
-        //change date to dateComing to display 2020-07-9
-        String result = ticketAmount + " " + "reserved for " + date + " under " + name;
+        String result = ticketAmount + " " + "reserved for " + formattedDate + " under " + lastName + ", " + firstName;
         System.out.println(result);
     }
 }
